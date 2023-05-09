c320436e151499160111671bfa2d949c5e377f1e
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-09 20:40:42 -0300

    5 Days Of Code

---
+++ src/main/java/org/example/typeofContructors.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class typeofContructors {
+
+    private String name;
+    private String lastname;
+    private int year;
+
+    public typeofContructors(String name, String lastname, int year) {
+        this.name = name;
+        this.lastname = lastname;
+        this.year = year;
+    }
+
+    public typeofContructors() {
+    }
+}
