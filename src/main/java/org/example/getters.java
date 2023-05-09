427bdddbab1ce3ee22869c22475c3ebe956187e6
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-09 20:43:10 -0300

    6 Days Of Code

---
+++ src/main/java/org/example/getters.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class getters {
+
+    private String name;
+    private String lastname;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+}
