0570ed6f01b602083d518c791d72759efcdd86ea
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-09 20:48:32 -0300

    9 Days Of Code

--- src/main/java/org/example/ObjectApp.java
+++ src/main/java/org/example/ObjectApp.java
@@ -3,6 +3,11 @@ package org.example;
 public class ObjectApp {
     public static void main(String[] args) {
         users u = new users();
+
+        u.setName("Jean");
+        u.setLastname("Miceli");
+
+        System.out.println("My name is: " + u.getName() + " and " + u.getLastname());
     }
 
 }
--- src/main/java/org/example/users.java
+++ src/main/java/org/example/users.java
@@ -1,4 +1,51 @@
 package org.example;
 
 public class users {
+
+    private String name;
+    private String lastname;
+    private int year;
+    private long id;
+
+    public users(String name, String lastname, int year, long id) {
+        this.name = name;
+        this.lastname = lastname;
+        this.year = year;
+        this.id = id;
+    }
+
+    public users() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
