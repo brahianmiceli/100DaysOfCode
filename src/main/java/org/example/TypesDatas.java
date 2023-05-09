a0fbaf69deab895f021283944fc06b4aa85116d1
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-09 20:37:20 -0300

    3 Days Of Code

---
+++ src/main/java/org/example/TypesDatas.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class TypesDatas {
+    private String name;
+    private int year;
+    private long phone;
+    private byte number1;
+    private float number2;
+    // short , double , char , boolean
+}
