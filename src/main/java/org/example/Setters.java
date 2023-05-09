8bb81b561f795e5c91035453939e501f24a0a028
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-09 20:43:47 -0300

    7 Days Of Code

---
+++ src/main/java/org/example/Setters.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Setters {
+    private String name;
+    private String lastname;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
