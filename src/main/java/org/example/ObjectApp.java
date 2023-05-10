3daddf476c29aae9d38b21fb9532584ce8a52e2b
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-10 20:54:52 -0300

    16 Days Of Code - utilizando nueva clase

--- src/main/java/org/example/ObjectApp.java
+++ src/main/java/org/example/ObjectApp.java
@@ -1,13 +1,46 @@
 package org.example;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ObjectApp {
     public static void main(String[] args) {
-        users u = new users();
 
-        u.setName("Jean");
-        u.setLastname("Miceli");
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("My name is: " + u.getName() + " and " + u.getLastname());
+        ArrayList<arraylistobj> aObj = new ArrayList<arraylistobj>();
+
+        System.out.println("Welcome to Store Food");
+        System.out.println("What do u want add? : ");
+        System.out.println("1" + " - Hamburgers and Coca Cola");
+        System.out.println("2" + " - Pizza And Coca cola");
+        System.out.println("3" + " - HotDog And Coca Cola");
+
+        int outClient = sc.nextInt();
+
+        switch (outClient) {
+            case 1:
+                System.out.println("Hamburgers and Coca Cola"
+                        + aObj.add(new arraylistobj("Hamburgers","Coca Cola"))
+                        + " - Ok, Added");
+                break;
+            case 2:
+                System.out.println("Pizza and Coca Cola"
+                        + aObj.add(new arraylistobj("Pizza","Coca Cola"))
+                        + " - Ok, Added"); // secuencia de sentencias.
+                break;
+            case 3 :
+                System.out.println("HotDog and Coca Cola"
+                        + aObj.add(new arraylistobj("Hot Dog","Cocacola"))
+                        + " - Ok, Added"); // secuencia de sentencias.
+                break;
+            default:
+                System.out.println("Invalid Character"); // Default secuencia de sentencias.
+        }
+
+
+
     }
 
 }
---
+++ src/main/java/org/example/arraylistobj.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class arraylistobj {
+    private String foodFirst;
+    private String waterFirst;
+
+
+    public arraylistobj(String foodFirst, String waterFirst) {
+        this.foodFirst = foodFirst;
+        this.waterFirst = waterFirst;
+    }
+
+    public String getFoodFirst() {
+        return foodFirst;
+    }
+
+    public void setFoodFirst(String foodFirst) {
+        this.foodFirst = foodFirst;
+    }
+
+    public String getWaterFirst() {
+        return waterFirst;
+    }
+
+    public void setWaterFirst(String waterFirst) {
+        this.waterFirst = waterFirst;
+    }
+
+
+
+
+}
