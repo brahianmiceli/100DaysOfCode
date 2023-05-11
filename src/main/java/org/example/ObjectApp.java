f0793c47bb996d5ac653823dd0a1b9b290f04698
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-10 21:49:25 -0300

    16 Days Of Code

--- src/main/java/org/example/ObjectApp.java
+++ src/main/java/org/example/ObjectApp.java
@@ -15,32 +15,26 @@ public class ObjectApp {
         System.out.println("What do u want add? : ");
         System.out.println("1" + " - Hamburgers and Coca Cola");
         System.out.println("2" + " - Pizza And Coca cola");
-        System.out.println("3" + " - HotDog And Coca Cola");
+        System.out.println("3" + " - HotDog And Coca Cola" );
 
         int outClient = sc.nextInt();
 
         switch (outClient) {
             case 1:
-                System.out.println("Hamburgers and Coca Cola"
-                        + aObj.add(new arraylistobj("Hamburgers","Coca Cola"))
-                        + " - Ok, Added");
+                aObj.add(new arraylistobj("Hamburgers","Coca Cola"));
+                System.out.println("Ok, Added : " + aObj.get(0).getFoodFirst() + " " + aObj.get(0).getWaterFirst());
                 break;
             case 2:
-                System.out.println("Pizza and Coca Cola"
-                        + aObj.add(new arraylistobj("Pizza","Coca Cola"))
-                        + " - Ok, Added"); // secuencia de sentencias.
+                aObj.add(new arraylistobj("Pizza","Coca Cola"));
+                System.out.println("Ok, Added : " + aObj.get(0).getFoodFirst() + " " + aObj.get(0).getWaterFirst());
                 break;
             case 3 :
-                System.out.println("HotDog and Coca Cola"
-                        + aObj.add(new arraylistobj("Hot Dog","Cocacola"))
-                        + " - Ok, Added"); // secuencia de sentencias.
+                aObj.add(new arraylistobj("HotDog","Coca Cola"));
+                System.out.println("Ok, Added : " + aObj.get(0).getFoodFirst() + " " + aObj.get(0).getWaterFirst());
                 break;
             default:
-                System.out.println("Invalid Character"); // Default secuencia de sentencias.
+                System.out.println("Invalid Character"); //
         }
-
-
-
     }
 
 }
