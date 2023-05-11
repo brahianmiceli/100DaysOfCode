7419f1b67d6c72fa5e190d69340f3c69eed1e06b
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-11 20:20:40 -0300

    17 Days Of Code

---
+++ src/main/java/org/example/AppFood.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AppFood {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<ArraylistObj> aObj = new ArrayList<ArraylistObj>();
+
+        System.out.println("Welcome to Store Food");
+        System.out.println("What do u want add? : ");
+        System.out.println("1" + " - Hamburgers and Coca Cola");
+        System.out.println("2" + " - Pizza And Coca cola");
+        System.out.println("3" + " - HotDog And Coca Cola" );
+
+        int outClient = sc.nextInt();
+
+        if (outClient<=1) {
+            aObj.add(new ArraylistObj("Hamburgers", "Coca Cola"));
+        }else if (outClient<=2){
+            aObj.add(new ArraylistObj("Pizza","Coca Cola"));
+        }else if (outClient<=3){
+            aObj.add(new ArraylistObj("HotDog","Coca Cola"));
+        }else{
+            System.out.println("Option Incorrect");
+        }
+
+        for (int i=0; i < aObj.size(); i++){
+            System.out.println("Food Added: "
+                    + aObj.get(i).getFoodFirst()
+                    + " and "
+                    + aObj.get(i).getWaterFirst());
+        }
+
+    }
+}
