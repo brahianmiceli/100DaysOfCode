9ac55e5401e9e9336908b938c744713eecdfc544
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-14 01:58:34 -0300

    18 Days Of Code

--- src/main/java/org/Practices/PracticesSimple/Github.java
+++ src/main/java/org/Practices/PracticesSimple/Github.java
@@ -2,5 +2,6 @@ package org.Practices.PracticesSimple;
 
 public class Github {
     //18 Days of Code
+    // Show days active in Github
     // https://twitter.com/jimiceli/status/1657165410557390848
 }
