f1997428490169e5c0fb9d88ab3f9f2680dbd059
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-14 02:01:50 -0300

    15 Days Of Code

--- src/main/java/org/Practices/PracticesSimple/Arraylist.java
+++ src/main/java/org/Practices/PracticesSimple/Arraylist.java
@@ -3,7 +3,7 @@ package org.Practices.PracticesSimple;
 
 import java.util.ArrayList;
 // PRACTICES WITH ARRAYLIST 
-public class Arraylist {
+public class Arraylist { //15 Days Of Code
     public static void main(String[] args) {
 
         ArrayList<String> f = new ArrayList<String>();
