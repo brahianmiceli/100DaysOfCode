b20a05b8d9e4e330293224ed1a6e4fbd4c833267
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-15 22:25:26 -0300

    21 Days Of Code

--- src/main/java/org/Practices/PracticesSimple/AbstractFactory.java
+++ src/main/java/org/Practices/PracticesSimple/AbstractFactory.java
@@ -3,8 +3,5 @@ package org.Practices.PracticesSimple;
 public class AbstractFactory { // Desing Patterns -> 20 Days of Code.
 
     // Practice with Design Patterns in Java.
-    // This moment, im practicing with Abstract Factory.
-
-
-    // https://github.com/jeanbmiceli/designPatternsinJava
+    // Practice the use Pros and Cros with Abstract Factory
 }
