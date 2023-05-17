17b15d5f4b9152a84a5821df3e38dd3e965857c1
jeanbmiceli <jeanbmiceli@gmail.com>
2023-05-17 16:52:37 -0300

    23 days of code - edit names

--- src/main/java/org/Practices/DesignPatterns/AbstractFactoryExample/ServiceDesign.java
+++ src/main/java/org/Practices/DesignPatterns/AbstractFactoryExample/ServiceDesign.java
@@ -3,22 +3,16 @@ public class ServiceDesign implements ServiceInformatics {
 
     @Override
     public void assignWork() {
-        System.out.println(
-                "1"
-        );
+        System.out.println("Our programmers have been informed of the program to be carried out.");
     }
 
     @Override
     public void indicateDeliveryDate() {
-        System.out.println(
-                "2"
-        );
+        System.out.println("The delivery date has been set as 03/25/2014.");
     }
 
     @Override
     public void informAboutPayment() {
-        System.out.println(
-                "3"
-        );
+        System.out.println("The amount to be paid will be proportional to the number of students who will use the software.");
     }
 }
--- src/main/java/org/Practices/DesignPatterns/AbstractFactoryExample/ServiceInformatics.java
+++ src/main/java/org/Practices/DesignPatterns/AbstractFactoryExample/ServiceInformatics.java
@@ -1,6 +1,6 @@
 package org.Practices.DesignPatterns.AbstractFactoryExample;
 
-public interface ServiceInformatics {
+public interface ServiceInformatics { //23 Days of code
     public void assignWork();
     public void indicateDeliveryDate();
     public void informAboutPayment();
